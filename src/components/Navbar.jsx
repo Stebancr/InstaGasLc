974ea16728824ad44e4e0e1d33d0d5b375a8fa65
Stebancr <luissteban9991@gmail.com>
2024-12-12 19:25:26 -0500

    first committ2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Styles/Navbar.css';
-import logo from '../../img/LOGO.svg'
+
 
 const Navbar = ({  }) => {
   ;
