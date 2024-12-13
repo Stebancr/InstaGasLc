7eccfaaa262e3b788f802fc46e70e18fc00a4950
Stebancr <luissteban9991@gmail.com>
2024-12-12 20:15:10 -0500

    first co

--- src/pages/Servicios.jsx
+++ src/pages/Servicios.jsx
@@ -7,21 +7,7 @@ const Servicios = () => {
   return (
     <div className="register-page">
       <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
-        </label>
-        <label>
-          Email:
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-        </label>
-        <label>
-          Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        </label>
-        <button type="submit" onClick={handleBack} className="btn">Register</button>
-      </form>
+      
     </div>
   );
 };
